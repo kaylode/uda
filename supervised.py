aa8a865138ed050af8fa0faf8b4a02d51aaab593
Kay Kieran <kayp.kieran@gmail.com>
2021-01-24 15:52:40 +0700

    add supervised

--- supervised.py
+++ supervised.py
@@ -1,28 +1,28 @@
 import os
-
 import time
 import argparse
-
+import numpy as np
 import torch
 from torch import nn
 from torch.backends import cudnn
-
 from torch.utils.tensorboard import SummaryWriter
-
+from configs import Config
 import dataset as dataset
 from efficientnet_pytorch import EfficientNet
 
 
 class Supervised_Trainer():
     def __init__(self, cfg):
-        self.model = EfficientNet.from_pretrained('efficientnet-b0')
+        self.model = EfficientNet.from_pretrained('efficientnet-b0',num_classes=10)
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1,)
-        self.epoch = cfg.epoch
+        self.num_epochs = cfg.num_epochs
         self.trainloader, self.valloader = dataset.cifar10_supervised_dataloaders()
         self.print_per_iter = 10
         self.iters = 0
-        self.num_iters = (num_epochs+1) * len(self.sup_trainloader)
+        self.epoch = 0
+        self.num_iters = (self.num_epochs+1) * len(self.trainloader)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def train_epoch(self):
         self.model.train()
@@ -37,12 +37,12 @@ class Supervised_Trainer():
             self.optimizer.zero_grad()
             inputs, targets = batch
             inputs = inputs.to(self.device)
-            targets = targets.squeeze(1).to(self.device)
+            targets = targets.to(self.device)
             outputs = self.model(inputs)
             loss = self.criterion(outputs, targets)
             loss.backward()
             self.optimizer.step()
-
+            
             end_time = time.time()
 
             running_time += (end_time - start_time)
@@ -77,16 +77,24 @@ class Supervised_Trainer():
                 loss = self.criterion(outputs, targets)
                 total_loss += loss.item()
                 preds = torch.argmax(outputs)
-                corrects += (pred == target).sum()
+                corrects += (preds == targets).sum()
                 sample_size += outputs.size(0)
         
         acc = corrects*1.0/sample_size
         total_loss /= sample_size
+
+        acc = np.round(acc, 5)
+        total_loss = np.round(total_loss, 5)
         print(f"Validation: Loss: {total_loss} || Accuracy: {acc}")
 
 
     def fit(self):
-        for epoch in range(self.epoch):
-            self.train_epoch()
+        for self.epoch in range(self.num_epochs):
+            # self.train_epoch()
             self.val_epoch()
     
+
+if __name__ == '__main__':
+    config = Config('configs/cfg.yaml')
+    trainer = Supervised_Trainer(config)
+    trainer.fit()
