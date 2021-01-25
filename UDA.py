801a4c8d7157063c426de47e9c3b1922570a5d49
Kay Kieran <kayp.kieran@gmail.com>
2021-01-25 11:33:59 +0700

    add additional techniques

--- UDA.py
+++ UDA.py
@@ -9,7 +9,7 @@ from torch.utils.tensorboard import SummaryWriter
 from configs import Config
 import dataset as dataset
 from efficientnet_pytorch import EfficientNet
-
+from loggers import Logger
 
 class Unsupervised_Trainer():
     def __init__(self, cfg):
@@ -17,11 +17,11 @@ class Unsupervised_Trainer():
         self.classes = cfg.classes
         self.model = EfficientNet.from_pretrained(cfg.model_name, num_classes=len(self.classes)).to(self.device)
         self.sup_criterion = nn.CrossEntropyLoss().to(self.device)
-        self.unsup_criterion = nn.KLDivLoss(reduction='batchmean').to(self.device)
+        self.unsup_criterion = nn.KLDivLoss(reduction='none').to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.01)
         self.num_epochs = cfg.num_epochs
         self.sup_trainloader, self.unsup_trainloader, self.unsup_aug_trainloader, self.valloader = dataset.cifar10_unsupervised_dataloaders(cfg)
-        self.batch_size = cfg.batch_size
+        self.sup_batch_size = cfg.sup_batch_size
         self.unsup_batch_size = cfg.unsup_batch_size
         self.sup_iter = iter(self.sup_trainloader)
         self.unsup_iter = iter(self.unsup_trainloader)
@@ -30,6 +30,8 @@ class Unsupervised_Trainer():
         self.iters = 0
         self.epoch = 0
         self.lamb = cfg.lamb
+        self.temperature = cfg.temperature if cfg.temperature > 0 else 1.
+        self.beta = cfg.beta
         self.num_iters = (self.num_epochs+1) * len(self.sup_trainloader)
         self.logger = Logger(log_dir=cfg.log_dir)
 
@@ -42,10 +44,10 @@ class Unsupervised_Trainer():
             "T": 0
         }
 
-        for idx, sup_batch in enumerate(self.unsup_aug_trainloader):
+        for idx, unsup_aug_batch in enumerate(self.unsup_aug_trainloader):
             start_time = time.time()
             self.optimizer.zero_grad()
-            unsup_aug_inputs, _ = sup_batch
+            unsup_aug_inputs, _ = unsup_aug_batch
 
             # Unsup batch
             try:
@@ -70,9 +72,28 @@ class Unsupervised_Trainer():
             unsup_aug_y_pred = self.model(unsup_aug_inputs)
             unsup_aug_y_probas = torch.log_softmax(unsup_aug_y_pred, dim=-1)
 
+            # confidence-based masking
+            if self.beta != 0:
+                unsup_loss_mask = torch.max(unsup_y_probas, dim=-1)[0] > self.beta
+                unsup_loss_mask = unsup_loss_mask.type(torch.float32)
+            else:
+                unsup_loss_mask = torch.ones(unsup_inputs.shape[0], dtype=torch.float32)
+            unsup_loss_mask = unsup_loss_mask.to(self.device)
+            
+            # temperature scaling
+            unsup_aug_y_probas = torch.log_softmax(unsup_aug_y_pred/self.temperature, dim=-1)
+
             unsup_loss = self.unsup_criterion(unsup_aug_y_probas, unsup_y_probas)
+            unsup_loss = torch.sum(unsup_loss, dim=-1)
+
             sup_loss = self.sup_criterion(sup_outputs, sup_targets)
-            loss = sup_loss + self.lamb * unsup_loss
+            
+            num_unsup = torch.sum(unsup_loss_mask, dim=-1)
+            if num_unsup == 0.:
+                loss = sup_loss            
+            else:
+                unsup_loss = torch.sum(unsup_loss * unsup_loss_mask, dim=-1) / torch.sum(unsup_loss_mask, dim=-1)
+                loss = sup_loss + self.lamb * unsup_loss
 
             loss.backward()
             self.optimizer.step()
@@ -95,6 +116,7 @@ class Unsupervised_Trainer():
                 running_time = 0
                 running_loss = {
                     "SUP": 0,
+                    "UNSUP":0,
                     "T": 0
                 }
 
@@ -110,6 +132,7 @@ class Unsupervised_Trainer():
         total_loss = 0
         with torch.no_grad():
             for idx, batch in enumerate(self.valloader):
+                print(batch)
                 inputs, targets = batch
                 inputs = inputs.to(self.device)
                 targets = targets.to(self.device)
@@ -130,7 +153,7 @@ class Unsupervised_Trainer():
 
 
     def fit(self):
-        print(f"Training semi-supervisedly with {len(self.sup_trainloader)} labelled and {len(self.unsup_trainloader)} unlabelled.")
+        print(f"Training semi-supervisedly with {len(self.sup_trainloader)*self.sup_batch_size} labelled and {len(self.unsup_trainloader)*self.unsup_batch_size} unlabelled.")
         for self.epoch in range(self.num_epochs):
             self.train_epoch()
             self.val_epoch()
